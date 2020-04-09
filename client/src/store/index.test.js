c8f30c5d5ec41fe11baa3aacc18f83bf92153794
Lukasz Rados <lukaszrados@gmail.com>
2020-04-09 13:04:09 +0200

    Update existing tests.

--- client/src/store/index.test.js
+++ client/src/store/index.test.js
@@ -9,45 +9,47 @@ const dummyCards = [1, 2, 3]
 
 const cardsState = {
     cards: {
-        1: {
-            cards: dummyCards
-        }
+        1: dummyCards
     }
 }
 
-test('updateLists updates list with a new one', () => {
-    const state = { ...emptyState }
-    const lists = {
-        1: {
-            cards: []
+describe('MUTATIONS', () => {
+    test('updateLists updates list with a new one', () => {
+        const state = { ...emptyState }
+        const lists = {
+            1: {
+                cards: []
+            }
         }
-    }
-    mutations.updateLists(state, lists)
-    expect(state.lists).toBe(lists)
+        mutations.updateLists(state, lists)
+        expect(state.lists).toBe(lists)
+    })
+
+    test('updateCards updates cards for a new list', () => {
+        const state = { ...emptyState }
+        const cards = [{}, {}]
+        const listId = 1
+        mutations.updateCards(state, { listId, cards })
+        expect(state.cards[listId]).toBe(cards)
+    })
+
+    test('updateCards updates cards for an existing list', () => {
+        const state = { ...emptyState }
+        const cards = [{}, {}]
+        const listId = 1
+        mutations.updateCards(state, { listId, cards })
+        expect(state.cards[listId]).toBe(cards)
+    })
 })
 
-test('updateCards updates cards for a new list', () => {
-    const state = { ...emptyState }
-    const cards = [{}, {}]
-    const listId = 1
-    mutations.updateCards(state, { listId, cards })
-    expect(state.cards[listId]).toBe(cards)
-})
+describe('GETTERS', () => {
+    test('getCardsInRandomOrder returns an empty array if non-existing list id passed', () => {
+        const state = { ...cardsState }
+        expect(getters.getCardsInRandomOrder(state)(4)).toEqual([])
+    })
 
-test('updateCards updates cards for an existing list', () => {
-    const state = { ...emptyState }
-    const cards = [{}, {}]
-    const listId = 1
-    mutations.updateCards(state, { listId, cards })
-    expect(state.cards[listId]).toBe(cards)
-})
-
-test('getCards returns an empty array if non-existing list id passed', () => {
-    const state = { ...cardsState }
-    expect(getters.getCards(state)(4)).toEqual([])
-})
-
-test('getCards returns cards from a list', () => {
-    const state = { ...cardsState }
-    expect(getters.getCards(state)(1).cards).toBe(dummyCards)
+    test('getCardsInRandomOrder returns cards from a list', () => {
+        const state = { ...cardsState }
+        expect(getters.getCardsInRandomOrder(state)(1)).toBe(dummyCards)
+    })
 })
